a3b527d0bffaecf4077da49c0ba84ad59ab1551a
Haya0024 <hayatemicrosoft@gmail.com>
2025-03-09 11:33:28 +0900

    Added the first server

---
+++ src/echo_server_single_thread.cpp
@@ -0,0 +1,105 @@
+#include <boost/asio.hpp>
+#include <boost/beast.hpp>
+#include <boost/beast/websocket.hpp>
+#include <iostream>
+#include <memory>
+
+
+namespace asio = boost::asio;
+namespace beast = boost::beast;
+namespace websocket = boost::beast::websocket;
+
+using tcp = asio::ip::tcp;
+using error_code = boost::system::error_code;
+
+class ChatSession : public std::enable_shared_from_this<ChatSession>{
+
+    websocket::stream<tcp::socket> ws_;
+    beast::flat_buffer buffer_;
+    
+    public:
+        explicit ChatSession(tcp::socket socket)
+            : ws_(std::move(socket))
+        {
+
+        }
+        
+        void start() {
+            ws_.async_accept(
+                [self = shared_from_this()](error_code ec) {
+                    if (!ec) {
+                        self->read();
+                    } else {
+                        std::cerr << "Error: " << ec.message() << std::endl;
+                    }
+            });
+        }
+    
+    private:
+        void read() {
+            ws_.async_read(buffer_,
+                [self = shared_from_this()](error_code ec, std::size_t bytes_transferred) {
+                    if (!ec) {
+                        std::string msg = beast::buffers_to_string(self->buffer_.data());
+                        std::cout << "Received: " << msg << std::endl;
+                        
+                        self->write(msg);
+                    } else {
+                        std::cerr << "Error: " << ec.message() << std::endl;
+                    }
+            });
+        }
+
+        void write(const std::string& msg) {
+            ws_.async_write(
+                asio::buffer(msg),
+                [self = shared_from_this()](error_code ec, std::size_t) {
+                    if (!ec) {
+                        self->buffer_.consume(self->buffer_.size());
+                        self->read();
+                    } else {
+                        std::cerr << "Write Error: " << ec.message() << std::endl;
+                    }
+            });
+        }
+};
+
+class WebSocketServer {
+
+    tcp::acceptor acceptor_;
+    
+    public:
+        WebSocketServer(asio::io_context& ioc, short port)
+            : acceptor_(ioc, tcp::endpoint(tcp::v4(), port)) {
+        }
+
+        void start() {
+            do_accept();
+        }
+    
+    private:
+        void do_accept() {
+            acceptor_.async_accept(
+                [this](error_code ec, tcp::socket socket) {
+                    if (!ec) {
+                        std::make_shared<ChatSession>(std::move(socket))->start();
+                    } else {
+                        std::cerr << "Error: " << ec.message() << std::endl;
+                    }
+                    do_accept();
+            });
+        }
+};
+
+int main() {
+    try {
+        asio::io_context ioc;
+        WebSocketServer server(ioc, 8080);
+        server.start();
+        ioc.run();
+    } catch (const std::exception& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+    }
+    
+    return 0;
+}
